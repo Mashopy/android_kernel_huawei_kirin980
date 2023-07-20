8938f1cb9c4e72031e2ea40f8eda79de42227305
Mashopy <eliasgheeraert@gmail.com>
2023-07-21 00:05:11 +0200

    drivers: connectivity: hisi: Remove dupplicated definition

--- drivers/connectivity/hisi/common/inc/plat_type.h
+++ drivers/connectivity/hisi/common/inc/plat_type.h
@@ -34,7 +34,6 @@ typedef unsigned int                uint32;
 typedef int                         int32;
 typedef unsigned long               uint64;
 typedef long                        int64;
-typedef unsigned long               ulong;
 
 #endif /* PLAT_TYPE_H */
 
